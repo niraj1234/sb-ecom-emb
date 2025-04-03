3dcd093372dc7d2b48232dc07bb4418eeaf101c2
niraj1234 <niraj.webtech@gmail.com>
2025-04-03 23:06:12 +0530

    03_APR_2025_23:05 | Before custome ResourceNotFoundException

--- src/main/java/com/ecommerce/project/controller/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/controller/com/ecommerce/project/exception/MyGlobalExceptionHandler.java
@@ -38,6 +38,11 @@ public class MyGlobalExceptionHandler {
     }
 
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message , HttpStatus.NOT_FOUND);
+    }
 
 }
 
