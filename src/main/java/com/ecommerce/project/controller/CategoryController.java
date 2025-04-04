9ac11780800067f41b4444bcbec8ad3f4e06d2fe
niraj1234 <niraj.webtech@gmail.com>
2025-04-04 12:51:13 +0530

    04_APR_2025_12:50 | Category DTO implemented using Model Mapper

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
 
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +21,9 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/category")
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> allCategories = categoryService.getAllCategories();
-        return ResponseEntity.status(HttpStatus.OK).body(allCategories);
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        return ResponseEntity.status(HttpStatus.OK).body(categoryResponse);
     }
 
     @PostMapping("/category")
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -3,13 +3,17 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.exception.APIException;
 import com.ecommerce.project.exception.NoCategoryFoundException;
 import com.ecommerce.project.exception.ResourceNotFoundException;
+import com.ecommerce.project.payload.CategoryDTO;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.model.Category;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -17,14 +21,23 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
-    public List<Category> getAllCategories() {
+    public CategoryResponse getAllCategories() {
         List<Category> savedCategories =  categoryRepository.findAll();
         if(savedCategories.isEmpty()){
             throw new NoCategoryFoundException("No Category available at this time");
         }
-        return savedCategories;
+        List<CategoryDTO> categoryDtos = savedCategories.stream()
+                .map( category -> modelMapper.map(category , CategoryDTO.class))
+                .collect(Collectors.toList());
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setContent(categoryDtos);
+        return categoryResponse;
     }
+
 
 
     @Override
