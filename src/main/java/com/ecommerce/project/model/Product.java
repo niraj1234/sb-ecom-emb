e7a2cda3a9a498d639771aa4691c0820fd9ed65c
niraj1234 <niraj.webtech@gmail.com>
2025-04-08 01:31:46 +0530

    Product controller service based on category

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,29 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productName;
+    private String description;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn
+    private Category category;
+
+}
